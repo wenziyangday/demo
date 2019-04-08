5d750b30f2cdac7a1ad8aa125d04e2b6c02083ef
闻紫阳 <wenzy@orderhandler.com>
2019-04-08 16:15:31 +0800

    调整结构

--- src/router/routers.js
+++ src/router/routers.js
@@ -7,21 +7,21 @@ import Home from '../components/Home';
 import Message from '../components/Message';
 
 const Root = () => (
-    <div>
-        <Switch>
-            <Route path="/" render={props => (
-                <App>
-                    <Switch>
-                        <Route path="/" exact component={Home}/>
-                        <Route path="/home" component={Home}/>
-                        <Route path="/test" component={Test}/>
-                        <Route path="/message/:id" component={Message}/>
-                        <Route render={() => <Redirect to="/"/>}/>
-                    </Switch>
-                </App>
-            )}/>
-        </Switch>
-    </div>
+	<div>
+		<Switch>
+			<Route path="/" render={props => (
+				<App>
+					<Switch>
+						<Route path="/" exact component={Home}/>
+						<Route path="/home" component={Home}/>
+						<Route path="/test" component={Test}/>
+						<Route path="/message/:id" component={Message}/>
+						<Route render={() => <Redirect to="/"/>}/>
+					</Switch>
+				</App>
+			)}/>
+		</Switch>
+	</div>
 );
 
 export default Root;
