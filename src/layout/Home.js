a167b493816c1fef0369d9cfaa0454742ec32190
wenziyang <1483319587@qq.com>
2019-04-08 23:52:28 +0800

    任务发布

--- src/layout/Home.js
+++ src/layout/Home.js
@@ -11,7 +11,6 @@ import Welcome from "../components/Welcome";
 import NoState from "../components/NoState";
 
 
-
 class Home extends Component {
 	render() {
 		return (
@@ -26,7 +25,9 @@ class Home extends Component {
 							点击跳转到路由参数search，state使用
 						</Link>
 						<br/>
-
+						<div>
+							任务：axios 使用、路由使用、redux使用、回顾一下文档
+						</div>
 						<b>常规组件之间的数据传输就是 父子组件信息传输</b> <br/>
 						<Welcome name="1.有状态组件"/>
 						<NoState name="2.无状态组件"/>
