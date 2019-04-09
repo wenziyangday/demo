7b57593dbc64ef0c61d88105081f90bfeea75068
闻紫阳 <wenzy@orderhandler.com>
2019-04-09 18:05:37 +0800

    猜测一下自己的设想

--- src/router/routers.js
+++ src/router/routers.js
@@ -6,6 +6,7 @@ import Test from '../business/Test';
 import Home from '../layout/Home';
 import Message from '../business/Message';
 import Login from '../business/Login';
+import About from '../business/About';
 
 const Root = () => (
 		<Switch>
@@ -14,6 +15,7 @@ const Root = () => (
 					<Switch>
 						<Route path="/" exact component={Home}/>
 						<Route path="/home" component={Home}/>
+						<Route path="/about" component={About}/>
 						<Route path="/test" component={Test}/>
 						<Route path="/message/:id" component={Message}/>
 						<Route path="/login" component={Login}/>
